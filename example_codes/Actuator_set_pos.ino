bf1a5efcf182d1c8fb9b9f459d0a4b6c6d653c5f
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2023-03-17 20:51:43 -0400

    Add actuator pos test file
    temp file for testing actuator controls

---
+++ example_codes/Actuator_set_pos.ino
@@ -0,0 +1,41 @@
+// Simple code to extend/retract actuator
+// This is for testing the actuator pitching angles - create new file for PID feedback control later - should have PID, hall effect read, current and voltage read, and actuator control as shown
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+const int buttonA = 3; // button 1 to extend actuator
+const int buttonB = 4; // button 2 to retract actuator
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(buttonA, INPUT_PULLUP); // set input A to pin 3
+  pinMode(buttonB, INPUT_PULLUP); // set input B to pin 4
+}
+
+void loop() {
+  if (digitalRead(buttonA) == LOW){
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      
+      Serial.print("Current Pos: ");
+      Serial.println(pos);
+      
+      delay(30);                       // waits 30ms for the servo to reach the position
+    }
+  }
+  if (digitalRead(buttonB) == LOW) {
+    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      
+      Serial.print("Current Pos: ");
+      Serial.println(pos);
+      
+      delay(30);                       // waits 30ms for the servo to reach the position
+    }
+  }
+}
