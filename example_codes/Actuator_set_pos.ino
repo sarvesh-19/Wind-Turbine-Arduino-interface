1e67b956312b008eb6f1ed17564bc5fd607ba40c
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2023-03-31 15:19:27 -0400

    Update Actuator_set_pos.ino

--- example_codes/Actuator_set_pos.ino
+++ example_codes/Actuator_set_pos.ino
@@ -1,53 +1,48 @@
-// Simple code to extend/retract actuator
-// This is for testing the actuator pitching angles - create new file for PID feedback control later - should have PID, hall effect read, current and voltage read, and actuator control as shown
+// 5 stages of position for the actuator
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
-const int buttonA = 3; // button 1 to extend actuator
-const int buttonB = 4; // button 2 to retract actuator
-int previous = 0; 
+const int buttonA = 3; // button 1 to cycle through actuator extension stages
+const int buttonB = 4; // button 2 to cycle through actuator retraction stages
+int currentPos = 0; // variable to store the current actuator position
+int stageIndex = 0; // variable to store the current stage index
+const int numStages = 5; // total number of stages
+const int stagePos[numStages] = {0, 45, 90, 135, 180}; // actuator position for each stage
+const int minPos = 0; // minimum actuator position
+const int maxPos = 180; // maximum actuator position
 
 void setup() {
   Serial.begin(9600);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   pinMode(buttonA, INPUT_PULLUP); // set input A to pin 3
   pinMode(buttonB, INPUT_PULLUP); // set input B to pin 4
+  myservo.write(currentPos); // set initial position to 0
 }
 
 void loop() {
-  if (digitalRead(buttonA) == LOW){
-    for (pos = previous; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      
-      Serial.print("Current Pos: ");
-      Serial.println(pos);
-      previous = pos;
-      
-      // break 
-      if (digitalRead(buttonA) == HIGH) {
-         break;
-        }
-      delay(30);                       // waits 30ms for the servo to reach the position
+  if (digitalRead(buttonA) == LOW) {
+    stageIndex++; // increment stage index
+    if (stageIndex >= numStages) { // if at last stage, loop back to first stage
+      stageIndex = 0;
     }
+    currentPos = stagePos[stageIndex]; // set current position to the next stage position
+    myservo.write(currentPos); // move the actuator to the current position
+    Serial.print("Current Pos: ");
+    Serial.println(currentPos);
+    delay(200); // debounce delay to prevent button bounce
   }
-  if (digitalRead(buttonB) == LOW) {
-    for (pos = previous; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      
-      Serial.print("Current Pos: ");
-      Serial.println(pos);
-      previous = pos;
 
-      // break
-      if (digitalRead(buttonB) == HIGH) {
-        break;
-       }
-      
-      delay(30);                       // waits 30ms for the servo to reach the position
+  if (digitalRead(buttonB) == LOW) {
+    stageIndex--; // decrement stage index
+    if (stageIndex < 0) { // if at first stage, loop back to last stage
+      stageIndex = numStages - 1;
     }
+    currentPos = stagePos[stageIndex]; // set current position to the previous stage position
+    myservo.write(currentPos); // move the actuator to the current position
+    Serial.print("Current Pos: ");
+    Serial.println(currentPos);
+    delay(200); // debounce delay to prevent button bounce
   }
 }
