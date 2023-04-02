c9fb8b1530a755a936541efa5ecb30518425ca55
SKrishnamoorthy <34112804+Vultimate1@users.noreply.github.com>
2023-04-02 13:27:49 -0400

    Update current_sensor.ino

--- current_sensor.ino
+++ current_sensor.ino
@@ -1,21 +1,24 @@
 #include <stdio.h>
-const int a1 = A1;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A0, OUTPUT);
-  Serial.begin(19200);
+  //pinMode(A0, OUTPUT);
+  Serial.begin(9600);
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  CurrentVal = 0;
-  AvgCurrent = 0;
+  float CurrentVal = 0.0;
+  float AvgCurrent = 0.0;
   for (int i=0; i<150; i++) {
     CurrentVal = analogRead(A0);
-    CurrentVal = CurrentVal*(5/1024)
+    //Serial.print("instantaneous current = ");
+    //Serial.println (CurrentVal);
+    CurrentVal = CurrentVal*(5.0/1024.0)*1000.0;
     AvgCurrent += CurrentVal;
   }
+  AvgCurrent = AvgCurrent/150.0;
+  Serial.print("Current = ");
   Serial.println(AvgCurrent);
 }
